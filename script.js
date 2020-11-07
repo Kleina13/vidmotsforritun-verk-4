94aa1e7e7c3d38db0cf96acb630fdedb6e9e17d6
Kleina13 <ragnarhelgi22@gmail.com>
2020-11-07 06:33:52 +0000

    model params

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+}
+
 navigator.getUserMedia = 
     navigator.getUserMedia || 
     navigator.webkitGetUserMedia ||
@@ -9,17 +17,26 @@ const canvas  = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 let model;
 
+function runDetection() {
+    model.detect(video)
+        .then(predictions => { console.log(predictions) })
+}
+
 handTrack.startVideo(video)
     .then(status => {
         if (status) { 
             navigator.getUserMedia(
                 { video: {} }, 
-                stream => { video.srcObject = stream },
+                stream => { 
+                    video.srcObject = stream 
+                    setInterval(runDetection, 1000)
+                },
                 err => console.log(err)
             );
         }
     });
 
 
-handTrack.load()
+
+handTrack.load(modelParams)
     .then(load_model => { model = load_model });
