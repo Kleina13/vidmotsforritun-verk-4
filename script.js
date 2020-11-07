fae5e5e25cc722dde897517daf91e761beb3c51c
Kleina13 <ragnarhelgi22@gmail.com>
2020-11-07 06:03:25 +0000

    testing

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// Notice there is no 'import' statement. 'handTrack' and 'tf' is
-// available on the index-page because of the script tag above.
+navigator.getUserMedia = 
+    navigator.getUserMedia || 
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
 
 const img = document.getElementById('img'); 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 // Load the model.
-handTrack.load().then(model => {
-    // detect objects in the image.
-    model.detect(img).then(predictions => {
-    console.log('Predictions: ', predictions); 
-    });
+handTrack.load()
+    .then(model => {
+        // detect objects in the image.
+        model.detect(img)
+            .then(predictions => { console.log('Predictions: ', predictions); });
 });
