afb42fe33c69e10db4903bd3ad1476a85fd368dd
Kleina13 <ragnarhelgi22@gmail.com>
2020-11-07 20:17:41 +0000

    comments

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+// paramaters
 const modelParams = {
     flipHorizontal: true,   // flip e.g for video 
     imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
     maxNumBoxes: 20,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.79,    // confidence threshold for predictions.
+    scoreThreshold: 0.79,   // confidence threshold for predictions.
 };
 
+// to get navigator working on every browser
 navigator.getUserMedia = 
     navigator.getUserMedia || 
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia;
 
+// vars
 const video  = document.querySelector('#video'); 
 const canvas = document.querySelector('#canvas');
 const ctx    = canvas.getContext('2d');
 let model;
 
-function runDetection() {
+// run detection teiknar það sem api sér og consle logar það 
+// (temp for the most part, útreiknigar koma hingað seinna)
+const runDetection = () => {
     model.detect(video)
         .then(predictions => { 
             console.log(predictions);
@@ -25,6 +30,7 @@ function runDetection() {
         });
 }
 
+// opna myndavél
 handTrack.startVideo(video)
     .then(status => {
         if (status) { 
@@ -32,13 +38,13 @@ handTrack.startVideo(video)
                 { video: {} }, 
                 stream => { 
                     video.srcObject = stream;
-                    setInterval(runDetection, 1000);
+                    setInterval(runDetection, 250); // run detection func á 1/4 af sekondu
                 },
                 err => console.log(err)
             );
         }
     });
 
-
+// onload function
 handTrack.load(modelParams)
     .then(lmodel => model = lmodel);
